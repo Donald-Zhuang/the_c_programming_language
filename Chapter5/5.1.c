2c9115f0b11a2c86806966380ba7d378edaa48e3
donald.zhuang <1034894966@qq.com>
2017-03-06 23:54:58 +0800

    for Chapter5 alloc.c

--- Chapter5/5.1.c
+++ Chapter5/5.1.c
@@ -12,65 +12,65 @@ int bufp = 0;
 
 char getch(void)
 {
-    if(bufp < 0)
-    {
-        //    printf("ERR: the buf is empty!\n");
-    }
-    return bufp > 0 ? buf[--bufp] : getchar();
+	if(bufp < 0)
+	{
+		//    printf("ERR: the buf is empty!\n");
+	}
+	return bufp > 0 ? buf[--bufp] : getchar();
 }
 void ungetch(char ch)
 {
-    if(bufp > BUFSIZ)
-        printf("ERR: the buf is full!\n");
-    else
-        buf[bufp++] = ch;
+	if(bufp > BUFSIZ)
+		printf("ERR: the buf is full!\n");
+	else
+		buf[bufp++] = ch;
 }
 
 int GetInt(int *num)
 {
-    char ch;
-    int sign = 0;
+	char ch;
+	int sign = 0;
 
-    //ignore the space character
-    while ( isspace(ch = getch()) )
-        ;
-    if ( !isdigit(ch) && ch != EOF && ch != '+' && ch != '-' )
-        return 0;
+	//ignore the space character
+	while ( isspace(ch = getch()) )
+		;
+	if ( !isdigit(ch) && ch != EOF && ch != '+' && ch != '-' )
+		return 0;
 
-    //calculate the digital number
-    sign = (ch == '-') ? -1 : 1;
-    if(ch == '-' || ch == '+')
-    {
-        int tmp = ch;
-        ch = getch();
-        if (!isdigit(ch))
-        {
-            ungetch(ch);
-//          ungetch(tmp);
-            return tmp;
-        }
-    }
-    for ( *num = 0; isdigit(ch); ch = getch() )
-        *num = *num * 10 + ch - '0';
-    *num *= sign;
+	//calculate the digital number
+	sign = (ch == '-') ? -1 : 1;
+	if(ch == '-' || ch == '+')
+	{
+		int tmp = ch;
+		ch = getch();
+		if (!isdigit(ch))
+		{
+			ungetch(ch);
+			//          ungetch(tmp);
+			return tmp;
+		}
+	}
+	for ( *num = 0; isdigit(ch); ch = getch() )
+		*num = *num * 10 + ch - '0';
+	*num *= sign;
 
-    //if it isn't the End Of Input, ungetch the character to buf
-    if(ch != EOF)
-        ungetch(ch);
+	//if it isn't the End Of Input, ungetch the character to buf
+	if(ch != EOF)
+		ungetch(ch);
 
-    return ch;
+	return ch;
 }
 
 int main(int argc,char *argv[])
 {
-    int i, num[255] = {0}, ret;
-    for(i = 0; ret != EOF && i < 255; i++)
-    {
-        ret = GetInt(&num[i]);
-        num[i] = (ret == '-' || ret == '+') ? 0 : num[i];
-    }
-    for( i-- ; i >= 0; i--)
-        printf("%d ", num[i]);
-    printf("\n");
-    return 0;
+	int i, num[255] = {0}, ret;
+	for(i = 0; ret != EOF && i < 255; i++)
+	{
+		ret = GetInt(&num[i]);
+		num[i] = (ret == '-' || ret == '+') ? 0 : num[i];
+	}
+	for( i-- ; i >= 0; i--)
+		printf("%d ", num[i]);
+	printf("\n");
+	return 0;
 }
--- Chapter5/alloc.c
+++ Chapter5/alloc.c
@@ -1,12 +1,11 @@
 /*************************************************************************
-	> File Name: alloc.c
-	> Author: donald.zhuang
-	> Mail: 1034894966@qq.com
-	> Created Time: Fri 03 Mar 2017 07:33:48 AM CST
+  > File Name: alloc.c
+  > Author: donald.zhuang
+  > Mail: 1034894966@qq.com
+  > Created Time: Fri 03 Mar 2017 07:33:48 AM CST
  ************************************************************************/
-
-#include<stdio.h>
-
+#include <stdio.h>
+#include <string.h>
 #define MAX_ALLOC_SIZE  (10000)
 static char allocbuf[MAX_ALLOC_SIZE] = {0};
 static char* allocp = allocbuf;
@@ -18,10 +17,11 @@ char *alloc_d(unsigned int length)
         return allocp - length;
     }
     else
-    {
+    { 
         return NULL;
     }
 }
+
 void free_d(char *p)
 {
     if(p < allocbuf + MAX_ALLOC_SIZE && p >= allocbuf )
@@ -56,12 +56,10 @@ unsigned ungetch(char ch)
 }
 
 #define MAX_LINE_LENGTH (255)
-static char getbuf[MAX_LINE_LENGTH] = {0};
 int getline_d(char *str, int maxline)
 {
     char ch;
     int i;
-
     for(i = 0; i < maxline && ( ch = getch() ) != EOF && ch != '\n'; i++)
     {
         *(str+i) = ch;
@@ -70,21 +68,70 @@ int getline_d(char *str, int maxline)
     {
         ungetch(ch);
     }
+    *(str + i) = '\0';
     return i;
 }
 
 #define MAX_INPUT_LINE (1000)
 static char *pline[MAX_INPUT_LINE] = {0};
-int readlines(char *str, int nline)
+int readlines(char *str[], int maxlines)
 {
-    int i;
-    for (i = 0; i < nline; i++)
-    {
+    int nlines = 0, len;
+    char *p, line[MAX_LINE_LENGTH];
 
+    while( (len = getline_d(line, MAX_LINE_LENGTH)) > 0)
+    {
+        if(nlines >= maxlines || (p = alloc_d(len)) == NULL )
+        {
+            return -1;
+        }
+        else
+        {
+            line[len - 1] = '\0';
+            strcpy(p, line);
+            str[nlines++] = p;
+        }
+    }
+    return nlines;
+}
+
+void writelines(char *str[], int nlines)
+{
+    while(nlines--)
+    {
+        printf("%s\n", *str++);
     }
 }
+
+void qsort_d(char *v[], int left, int right)
+{
+    int i, last;
+    void swap(char *v[], int i, int j);
+    if(left > right)
+        return;
+    swap(v, left, (left + right)/2);
+    last = left;
+    for(i = left + 1; i <= right; i++)
+    {
+        if(strcmp(v[i], v[left]) < 0)
+        {
+            swap(v, ++last, i);
+        }
+    }
+    swap(v, left, last);
+    qsort_d(v, left, last - 1);
+    qsort_d(v, last+1, right);
+}
+
+void swap(char *v[], int i, int j)
+{
+    char *temp;
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
 int main(int argc, char **argv)
 {
-
     return 0;
 }
